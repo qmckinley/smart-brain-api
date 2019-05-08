1d19f73756c0804eb69ff4ed119731935cbad29f
Q.McKinley <q.mckinley@outlook.com>
2019-05-08 16:05:49 -0400

    fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true
   }
 });
@@ -29,7 +29,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log('app is running on port ' + port);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
